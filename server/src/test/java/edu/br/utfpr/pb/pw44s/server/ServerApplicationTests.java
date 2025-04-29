7f1dbcc8a4f53b599a26fd8127b13aa4a648556b
Lucas Botezini <114296072+Lucas-Botezini@users.noreply.github.com>
2025-04-29 20:19:47 -0300

    Project Created with Spring Initializer

---
+++ server/src/test/java/edu/br/utfpr/pb/pw44s/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.br.utfpr.pb.pw44s.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
